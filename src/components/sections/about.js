3bfff7cf97845666070443ef60d7a0e79ab4bedc
huzaifi0604 <huzzaifaasim@gmail.com>
2023-03-29 21:43:45 +0500

    intro

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,10 +135,12 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              My name is Muhammad Huzaifa and I am a highly motivated and detail-oriented student
+              currently pursuing a bachelor's degree in Cybersecurity and Digital Forensics from
+              FAST - NUCES. Throughout my academic career, I have gained extensive knowledge in
+              programming languages such as Python, C++, and Java. In addition, I have also
+              developed skills in Malware Analysis and SOC Analysis, which have equipped me to
+              effectively identify and remediate security threats.
             </p>
 
             <p>
