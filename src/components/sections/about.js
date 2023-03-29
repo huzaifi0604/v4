cf5bd3d137fb536f2cceffdf5165c608b7b5e384
huzaifi0604 <huzzaifaasim@gmail.com>
2023-03-29 21:38:20 +0500

    Reverting About Me!
    This reverts commit e20d265f0d94d1630eab40ce0c7af6c2674751a1.

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,43 +135,32 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              I am a highly motivated and detail-oriented student currently pursuing a bachelor's
-              degree in Cybersecurity and Digital Forensics from FAST - NUCES. Throughout my
-              academic career, I have gained extensive knowledge in programming languages such as
-              Python, C++, and Java. In addition, I have also developed skills in Malware Analysis
-              and SOC Analysis, which have equipped me to effectively identify and remediate
-              security threats.
+              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
+              interest in web development started back in 2012 when I decided to try editing custom
+              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
+              about HTML &amp; CSS!
             </p>
+
             <p>
-              In my spare time, I enjoy participating in online cybersecurity competitions and
-              staying up-to-date with the latest cybersecurity trends and techniques. I am always
-              eager to learn and take on new challenges, and I am confident that my strong work
-              ethic and passion for cybersecurity will make me a valuable asset to any team. If you
-              are interested in connecting or learning more about my experience, please don't
-              hesitate to reach out.
+              Fast-forward to today, and I’ve had the privilege of working at{' '}
+              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
+              <a href="https://starry.com/">a start-up</a>,{' '}
+              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
+              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
+              main focus these days is building accessible, inclusive products and digital
+              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
+              clients.
             </p>
-            <p>
-              During this tenure I got the privilege to hone my skills at{' '}
-              <br>
-                <a href="https://www.linkedin.com/posts/huzaifi0604_internship-completion-certificate-activity-6983426377731457024-0uyK?utm_source=share&utm_medium=member_desktop">
-                  Cyber Defense &amp;Network Security intern at Thincscorp
-                </a>
-              </br>
-              ,{' '}
-              <br>
-                <a href="https://www.linkedin.com/posts/huzaifi0604_csl-blueteam-cybersecurity-activity-6979030619301273601-IFTx?utm_source=share&utm_medium=member_desktop">
-                  Blue Team Trainer @ CSL, NUCES
-                </a>
-              </br>
-              , <br>DataBase Systems &amp; Data Structures' Lab Demonstrator at FAST - NUCES</br>.
-            </p>
+
             <p>
               I also recently{' '}
               <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                published a Blog
+                launched a course
               </a>{' '}
-              on exploiting window's SMB module using Eternal Blue through Metasploit.
+              that covers everything you need to build a web app with the Spotify API using Node
+              &amp; React.
             </p>
+
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
 
